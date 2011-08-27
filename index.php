896d751cc1b11100227d4107ca4cc1800e146551
Khebs Tenorio <khebs@live.com>
2011-08-27 18:09:50 +0800

    Change lots of files, remove some files, updated some files, added cool scripts.

--- index.php
+++ index.php
@@ -1,32 +1,14 @@
-<?php
-	define('ALLOW_ADMIN_IP', '127.0.0.1');
-
-	if ($_SERVER['REMOTE_ADDR'] === ALLOW_ADMIN_IP)
-		$show_options = TRUE;
-	else
-		$show_options = FALSE;
-?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-status-bar-style" content="black">
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 		<title>Docman v0.5 | SystemsAcumen</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">		
 		<link rel="icon" type="image/png" href="assets/img/favicon.png"/>
 		<!-- Basic Styling -->
 		<link rel="stylesheet" href="assets/css/style.css"/>
-		<!-- Dependencies -->
-		<script type="text/javascript" src="lib/vendor/jquery/jquery.js"></script>
-		<script type="text/javascript" src="lib/vendor/jquery/plugins/hotkeys.js"></script>
-		<script type="text/javascript" src="lib/vendor/jquery/plugins/scroll-startstop.events.js"></script>
-		<script type="text/javascript" src="lib/vendor/underscore/underscore.js"></script>
-		<script type="text/javascript" src="lib/vendor/underscore/plugins/string.js"></script>
-		<script type="text/javascript" src="lib/vendor/backbone/backbone.js"></script>
-		<script type="text/javascript" src="lib/vendor/json2.js"></script>
-		<script type="text/javascript" src="lib/vendor/namespace.js"></script>
-		<script type="text/javascript" src="lib/vendor/proper.js"></script>
+		<!-- Scripts -->
+		<script type="text/javascript" src="lib/vendor/head.js"></script>
+		<script type="text/javascript" src="lib/docman/Docman.js"></script>
 	</head>
 	<body>
 		<div id="header">
@@ -48,36 +30,11 @@
 			</div>
 		</div>
 		<div id="sidebar"></div>
-		<?php if ($show_options === TRUE):
-		?>
 		<ul id="docman-options"></ul>
-		<?php endif;?>
 
 		<div id="scrollable">
-			<div id="content" class="smooth <?=($show_options === TRUE) ? "admin-enabled" : "";?>"></div>
+			<div id="content" class="smooth admin-enabled"></div>
 		</div>
 	</body>
-	<!-- Docman -->
-	<script type="text/javascript" src="lib/docman/Docman.js"></script>
-	<script type="text/javascript" src="lib/docman/router/App.js"></script>
-	<script type="text/javascript" src="lib/docman/router/Doc.js"></script>
-	<script type="text/javascript" src="lib/docman/router/Search.js"></script>
-	<script type="text/javascript" src="lib/docman/view/SearchForm.js"></script>
-	<script type="text/javascript" src="lib/docman/view/Sidebar.js"></script>
-	<script type="text/javascript" src="lib/docman/view/Content.js"></script>
-	<script type="text/javascript" src="lib/docman/model/Docs.js"></script>
-	<script type="text/javascript" src="lib/docman/model/Menus.js"></script>
-	<!-- Docman Admin -->
-	<?php if ($show_options === TRUE):
-	?>
-	<script type="text/javascript" src="lib/docman/admin/Router.js"></script>
-	<script type="text/javascript" src="lib/docman/admin/View.js"></script>
-	<script type="text/javascript" src="lib/docman/admin/ViewAdd.js"></script>
-	<script type="text/javascript" src="lib/docman/admin/ViewModifyDoc.js"></script>
-	<script type="text/javascript" src="lib/docman/admin/ViewSettings.js"></script>
-	<?php endif;?>
-	<script type="text/javascript">
-		Docman.start();
-
-	</script>
+	
 </html>
--- storage/index.php
+++ storage/index.php
@@ -9,7 +9,7 @@
 	}
 	else
 	{
-		include_once ("class/sqlitepdo.class.php");
+		include ('class/class.db.php');
 	}
 
 	try
@@ -19,19 +19,21 @@
 		@list($table, $id) = preg_split('/\//', substr($path_info, 1));
 
 		//----------------------------------------------------------------------------
-		// Prepare PDO SQLite
+		// Prepare PDO
 		//----------------------------------------------------------------------------
 
 		$sql = null;
+		$sqliterror = null;
 
 		#db file location
-		$dbfile = dirname(realpath(__FILE__)) . DS . 'db' . DS . 'docman';
+		$dbfile = dirname(realpath(__FILE__)) . DS . 'db' . DS . 'docman.sqlite';
 
 		#create new sqlite database object($dblink)
-		$dblink = new sqlitePDO($dbfile, "sqlite");
+		$sqlite = new db('sqlite:' . $dbfile);
 
-		#connect to sqlite3 database
-		$dblink -> connect();
+		#Error?
+		if ($sqliterror !== null)
+			die($sqliterror);
 
 		//----------------------------------------------------------------------------
 		#backbone emulateHTTP $_POST['_method']
@@ -44,31 +46,15 @@
 
 		if ($method === 'GET')
 		{
-			$sql = "SELECT * FROM $table";
-			if (isset($id) && $id)
-				$sql .= " WHERE id = $id";
+			$result = $sqlite -> select($table, ( ! empty($id) ? "ids = $id" : ''));
 		}
 
-		//----------------------------------------------------------------------------
-		// Process Data
-		//----------------------------------------------------------------------------
+		# deadly silent error :D will fixed soon, just to make things work right now
+		if ( ! $result)
+			$result = array();
 
-		if ($sql)
-		{
-			$result = $dblink -> query($sql);
-			$data = array();
-
-			if ($result > 0)
-			{
-				while ($row = $dblink -> fetcharray())
-				{
-					$data[] = $row;
-				}
-			}
-
-			// sleep(rand(1,5));
-			die(json_encode($data));
-		}
+		//sleep(rand(1,5));
+		die(json_encode($result));
 	}
 	catch (PDOException $e)
 	{
